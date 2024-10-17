364ef154430b761eeb6452b4ce9b3cabd74e38b8
E13-star <evelen.libras@gmail.com>
2024-10-16 21:30:30 -0300

    Add files via upload
    Atividade-05

---
+++ Evelen/ex_3.Igor.py
@@ -0,0 +1,11 @@
+numero = float(input("Informe um  numero:"))
+
+
+if numero > 0 :
+    print ("O numero é positivo .")
+    
+elif numero == 0 :
+    print("O numero é zero.")
+
+else:
+    print("O numero é negativo.")
